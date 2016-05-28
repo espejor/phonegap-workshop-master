235574be0ce3067d72836b3d58e9f9c560a1dd81
espejor <espejor@gmail.com>
2016-05-28 22:22:48 +0200

    no message

--- js/main.js
+++ js/main.js
@@ -1,31 +1,21 @@
 var app = {
-    showAlert: function (message, title) {
-      if (navigator.notification) {
-          navigator.notification.alert(message, null, title, 'OK');
-      } else {
-          alert(title ? (title + ": " + message) : message);
-      }
-    },
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
 
-    initialize: function() {
+  showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    }
+  },
+
+
+
+  initialize: function() {
       var self = this;
       this.store = new MemoryStore(function() {
-          self.showAlert('Store Initialized', 'Info');
+          $('body').html(new HomeView(self.store).render().el);
       });
-      $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+  }
 
 };
 
